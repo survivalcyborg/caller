3915c1699befeaa686133a01d754c637f98f4610
survivalcyborg <119539298+survivalcyborg@users.noreply.github.com>
2024-07-12 12:33:47 -0400

    Create test.rb

---
+++ test.rb
@@ -0,0 +1,18 @@
+class Calculator
+  def add(a, b)
+    a + b
+  end
+
+  def subtract(a, b)
+    a - b
+  end
+
+  def multiply(a, b)
+    a * b
+  end
+
+  def divide(a, b)
+    raise ArgumentError, "Division by zero" if b == 0
+    a / b
+  end
+end
